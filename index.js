69e42e915d5bbb6ca5e35dc8d92757dc42968643
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-05-06 19:20:12 +0100

    set up ready

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
-
+const cors = require('cors');
+app.use(cors());
+const users =[
+    {"id":1,"name":'Nahid',"email":'nahid@gmail.com'},
+    {"id":2,"name":'Nabir',"email":'nabir@gmail.com'}
+]
 app.get('/',(req,res)=>{
     res.send('User Management Server is running')
+})
+app.get('/users',(req,res)=>{
+    res.send(users)
 })
 
 app.listen(port,()=>{
