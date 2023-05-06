235e6e4418407893b08ce350ac32353f0c69e09c
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-05-06 20:22:46 +0100

    post done

--- index.js
+++ index.js
@@ -3,20 +3,27 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 app.use(cors());
-const users =[
-    {"id":1,"name":'Nahid',"email":'nahid@gmail.com'},
-    {"id":2,"name":'Nabir',"email":'nabir@gmail.com'}
+app.use(express.json())
+const users = [
+    { "id": 1, "name": 'Nahid', "email": 'nahid@gmail.com' },
+    { "id": 2, "name": 'Nabir', "email": 'nabir@gmail.com' }
 ]
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('User Management Server is running')
 })
-app.get('/users',(req,res)=>{
+app.get('/users', (req, res) => {
     res.send(users)
 })
-app.post('/users',(req,res)=>{
-    console.log(req.body);
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    console.log(newUser);
+    res.send(newUser)
+
+
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is running on PORT: ${port}`);
 })
